c23bd57bd07f32eb89535e22fb21dce224bc75b5
Joe-wonho <whdnjsgh22@naver.com>
2023-05-02 18:09:31 +0900

    style: login route path 설정

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<LoginPage />} />
+        <Route path='/login' element={<LoginPage />} />
         {/* <Route path='/signup' element={<SignupPage />} /> */}
       </Routes>
     </BrowserRouter>
