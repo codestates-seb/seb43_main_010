79c512d545eb50e30675bea8d7ccf5a9b0817b20
SEBFE43GwakMiso <kms30922@naver.com>
2023-05-03 15:29:48 +0900

    style: music 페이지 레이아웃 제작

--- client/src/components/Music/Music.jsx
+++ client/src/components/Music/Music.jsx
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 import styled, { keyframes, css } from 'styled-components';
 import cdImg from '../../assets/png-file/cd-img.png';
 import bookMark from '../../assets/svg-file/bookmark.svg';
-// import moonVerified from '../../assets/svg-file/moon-verified-icon.svg';
+import moonVerified from '../../assets/svg-file/moon-verified-icon.svg';
 import playIcon from '../../assets/svg-file/play-icon.svg';
 import stopIcon from '../../assets/svg-file/stop-icon.svg';
 import profileImg from '../../assets/jpg-file/profile-img.jpg';
 import Head from '../Head/Head';
 import Nav from '../Nav/Nav';
-// import Footer from './Footer';
+// import Footer from '../Footer/Footer';
 
 const Body = styled.div`
   height: 100vh;
@@ -24,6 +24,9 @@ const MusicContainer = styled.div`
   justify-content: center;
   height: 100%;
   margin-left: 218px;
+  @media screen and (max-width: 1024px) {
+    margin-left: 0;
+  }
   @media screen and (min-width: 768px) {
     flex-direction: row;
     justify-content: center;
@@ -48,6 +51,10 @@ const CdImgBox = styled.div`
           ${spin} 20s linear infinite
         `
       : 'none'};
+  @media screen and (max-width: 1024px) {
+    width: 350px;
+    height: 350px;
+  }
   @media screen and (min-width: 768px) {
     background-size: cover;
     background-position: center;
@@ -65,19 +72,24 @@ const IconWrapper = styled.div`
   cursor: pointer;
 `;
 
-const MusicBox = styled.div`
+const AritistBox = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   width: 519px;
   margin-left: 68px;
   position: relative;
+  @media screen and (max-width: 1024px) {
+    margin-left: 0;
+    align-items: center;
+    text-align: center;
+  }
   @media screen and (min-width: 768px) {
     justify-content: center;
   }
 `;
 
-const MusicTitle = styled.div`
+const MusicBox = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -89,6 +101,30 @@ const MusicTitle = styled.div`
   font-size: 17px;
   font-weight: bold;
   color: var(--gray-blue-300);
+  @media screen and (max-width: 1024px) {
+    width: 80%;
+    margin: 0 auto;
+    margin-top: 20px;
+  }
+`;
+
+const MusicInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 10px;
+`;
+
+const MusicTitle = styled.span`
+  font-size: 16.5px;
+  font-weight: bold;
+  color: var(--white-100);
+`;
+
+const MusicSinger = styled.span`
+  font-size: 16px;
+  font-weight: 500;
+  color: var(--gray-blue-300);
 `;
 
 const BookMark = styled.img`
@@ -97,30 +133,56 @@ const BookMark = styled.img`
   position: absolute;
   right: 173px;
   top: 15px;
+  @media screen and (max-width: 1024px) {
+    right: 10px;
+  }
+`;
+
+const MusicBoxImg = styled.img`
+  width: 46px;
+  height: 46px;
+  margin-right: 10px;
 `;
 
 const TextBox = styled.div`
   display: flex;
-  align-items: center;
+  flex-direction: row;
+  align-items: flex-start;
   width: 519px;
   height: 162px;
   background-color: var(--black-900);
   border-radius: 15px;
-  padding: 0 18px;
+  padding: 18px 18px;
   font-size: 13px;
   color: var(--gray-blue-300);
   margin-top: 12px;
+  @media screen and (max-width: 1024px) {
+    width: 100%;
+    margin-top: 20px;
+  }
   @media screen and (min-width: 768px) {
     width: 100%;
     max-width: 768px;
   }
 `;
 
-const ProfileImage = styled.img`
+const TextBoxImg = styled.img`
   width: 46px;
   height: 46px;
   border-radius: 50%;
   margin-right: 10px;
+`;
+
+const UserName = styled.span`
+  font-size: 16px;
+  font-weight: bold;
+  color: var(--white-100);
+  margin-right: 5px;
+`;
+
+const MoonVerified = styled.img`
+  width: 10px;
+  height: 12px;
 `;
 
 const Text = styled.div`
@@ -128,6 +190,7 @@ const Text = styled.div`
   white-space: pre-wrap; //텍스트 줄 바꿈 유지
   font-size: 13px;
   line-height: 112.5%;
+  margin-top: 30px;
   @media screen and (min-width: 768px) {
     width: 100%;
   }
@@ -135,6 +198,11 @@ const Text = styled.div`
 
 const Music = () => {
   const [isPlaying, setIsPlaying] = useState(false);
+  // const [profileImage, setProfileImage] = useState(profileImg);
+  // const [musicTitle, setMusicTitle] = useState('');
+  // const [musicSinger, setMusicSinger] = useState('');
+  // const [userName, setUserName] = useState('');
+  // const [textValue, setTextValue] = useState('');
 
   const handlePlayToggle = () => {
     setIsPlaying(!isPlaying);
@@ -150,21 +218,26 @@ const Music = () => {
             <img src={isPlaying ? stopIcon : playIcon} alt={isPlaying ? 'stop' : 'play'} />
           </IconWrapper>
         </CdImgBox>
-        <MusicBox>
-          <MusicTitle>
-            <ProfileImage src={profileImg} alt='profile' />
-            Love Over And Over Again
-          </MusicTitle>
+        <AritistBox>
+          <MusicBox>
+            <MusicBoxImg src={profileImg} alt='profile' />
+            <MusicInfo>
+              <MusicTitle>Love Over And Over Again</MusicTitle>
+              <MusicSinger>Switch</MusicSinger>
+            </MusicInfo>
+          </MusicBox>
           <BookMark src={bookMark} alt='bookmark' />
           <TextBox>
-            <ProfileImage src={profileImg} alt='profile' />
+            <TextBoxImg src={profileImg} alt='profile' />
+            <UserName>뷔</UserName>
+            <MoonVerified src={moonVerified} alt='verified' />
             <Text>
               저는 여유와 힐링이 느껴지는 곡이 좋습니다. 어렸을 때 부터 그런 여유있는 곡들을 많이 들어왔어가지고 여러분들께도 같이 공유하고 소개시켜
               드리고 싶었습니다. Switch의 love over and again 이라는 곡이예요. 제가 이런 노래를 들을때는 뭔가 어.. 여유를 가지고 싶고 나름의 힐링도
               가지고 싶고 뭔가 내적 댄스도 추고 싶은 그런 LP에 있으면 좋을법한 음악이예요.
             </Text>
           </TextBox>
-        </MusicBox>
+        </AritistBox>
       </MusicContainer>
       {/* <Footer /> */}
     </Body>
