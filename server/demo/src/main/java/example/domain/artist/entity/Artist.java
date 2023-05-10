62f575a95435c3062a1ac053614884bf2a72cba5
HJKKIM <hoj3646@naver.com>
2023-05-10 11:57:14 +0900

    fix: sql 문법 오류 해결

--- server/demo/src/main/java/example/domain/artist/entity/Artist.java
+++ server/demo/src/main/java/example/domain/artist/entity/Artist.java
@@ -34,7 +34,7 @@ public class Artist extends BaseEntity {
     @Column(nullable = true)
     private String profile = "https://mblogthumb-phinf.pstatic.net/MjAyMDA2MTBfMTY1/MDAxNTkxNzQ2ODcyOTI2.Yw5WjjU3IuItPtqbegrIBJr3TSDMd_OPhQ2Nw-0-0ksg.8WgVjtB0fy0RCv0XhhUOOWt90Kz_394Zzb6xPjG6I8gg.PNG.lamute/user.png?type=w800";
 
-    @Column(nullable = false)
+    @Column(name = "\"group\"", nullable = false)
     private String group;
 
     @Column(nullable = true)
