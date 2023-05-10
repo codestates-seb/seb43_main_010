d8525ca2b0cf0be1c2a0734c9a953c3d38cdb005
SEBFE43GwakMiso <kms30922@naver.com>
2023-05-10 15:30:59 +0900

    style: Footer 수정

--- client/src/pages/MusicPage.jsx
+++ client/src/pages/MusicPage.jsx
@@ -1,23 +1,23 @@
 import styled from 'styled-components';
 import Head from '../components/Head/Head';
 import Music from '../components/Music/Music';
-// import Foot from '../components/Foot/Foot';
+import MiniFoot from '../components/Foot/MiniFoot';
 
 const MusicPageBlock = styled.div`
   min-height: 100vh;
   background-color: var(--dark-blue-900);
 `;
 
-// const StyledFoot = styled(Foot)`
-//   background-color: var(--dark-blue-900);
-// `;
+const StyledFoot = styled(MiniFoot)`
+  background-color: var(--dark-blue-900);
+`;
 
 const MusicPage = () => {
   return (
     <MusicPageBlock>
       <Head />
       <Music />
-      {/* <StyledFoot /> */}
+      <StyledFoot />
     </MusicPageBlock>
   );
 };
