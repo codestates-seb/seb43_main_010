1a427fe48da464cce28f5af7df74bf971b864504
Choi-hyeona <xhrl523@naver.com>
2023-05-11 18:01:38 +0900

    style: 아티스트 대표 이미지 수정 및 25개 컬러 지정

--- client/src/components/Feed/FeedMaterial/Gradation.jsx
+++ client/src/components/Feed/FeedMaterial/Gradation.jsx
@@ -8,7 +8,7 @@ const RealGradation = styled.div`
   width: 100%;
   height: 135px;
   /* background 색상은 나중에 props로 바꾸어주어야 할 부분임 */
-  background: linear-gradient(to top, transparent, #c7e7ff);
+  background: linear-gradient(to top, transparent, #c5d6eb);
 `;
 
 const Gradation = () => {
