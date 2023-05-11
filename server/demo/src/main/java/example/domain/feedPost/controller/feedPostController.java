4ae00886314b2e1fcbd588a73defd1ad48486b2b
HJKKIM <hoj3646@naver.com>
2023-05-11 11:33:16 +0900

    refactor: feedPost 조회 무한 스크롤 개선

--- server/demo/src/main/java/example/domain/feedPost/controller/feedPostController.java
+++ server/demo/src/main/java/example/domain/feedPost/controller/feedPostController.java
@@ -8,8 +8,10 @@ import example.domain.feedPost.dto.feedPostDto;
 import example.domain.feedPost.entity.FeedPost;
 import example.domain.feedPost.mapper.feedPostMapper;
 import example.domain.feedPost.service.feedPostService;
+import example.global.response.MultiResponseDto;
 import example.global.response.SingleResponseDto;
 import example.domain.feedPost.repository.feedPostRepository;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -44,14 +46,6 @@ public class feedPostController {
                 HttpStatus.OK);
     }
 
-//    @GetMapping("/{group_id}")
-//    public ResponseEntity getFeedPost(@PathVariable("group_id") @Positive int feedId){
-//        feedPost feedPost = service.findFeedPost(feedId);
-//        List<feedPost> feedPosts = feedPosts.getContent();
-//        return new ResponseEntity<>(
-//                new MultiResponseDto<>(mapper.feedPostsToFeedResponseDtos(feedPosts),
-//                        HttpStatus.OK);
-//    }
 
     // feed 상세 조회
     @GetMapping("{feed-id}")
@@ -65,34 +59,16 @@ public class feedPostController {
 
 
     // feed 리스트 조회(무한 스크롤)
-    @GetMapping("/{group_id}")
-    public ResponseEntity<List<FeedPost>> getFeeds(@RequestParam(value = "lastId", required = false) Integer lastId) {
-        int pageSize = 10; // 한 페이지에 보여줄 데이터의 양
-        List<FeedPost> feedPosts;
-        if (lastId == null) {
-            feedPosts = feedPostRepository.findFirst10ByOrderByIdDesc(); // 마지막 10개의 피드 게시물 반환
-        } else {
-            feedPosts = feedPostRepository.findByIdLessThanOrderByIdDesc(lastId, PageRequest.of(0, pageSize)); // 마지막 id보다 작은 항목 반환
-        }
-        return ResponseEntity.ok(feedPosts);
+    @GetMapping("/new_post")
+    public ResponseEntity getAllFeedPost(@RequestParam(defaultValue = "1") @Positive int page,
+                                         @RequestParam(defaultValue = "16") @Positive int size) {
+        Page<FeedPost> feedPosts = service.findAllFeedPost(page -1, size);
+        List<FeedPost> list = feedPosts.getContent();
+
+        return new ResponseEntity(new MultiResponseDto<>(mapper.feedPostsToFeedResponseDtos(list), feedPosts), HttpStatus.OK);
     }
 
 
-
-//    // feed 리스트 조회(무한 스크롤)
-//    @PatchMapping("/{feed_id}")
-//    public ResponseEntity pathchFeedPost(@PathVariable("feed_id") @Positive int feedPostId,
-//                                        @Valid @RequestBody feedPostDto.Patch requestBody) {
-//        requestBody.setFeedPostId(feedPostId);
-//        Fans fans = fansRepository.findById(requestBody.getFansId())
-//                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.FANS_NOT_FOUND));
-//        feedPost feedPost = mapper.feedPatchDtoToFeed(requestBody, fans);
-//        feedPost updateFeedPost = service.updateFeedPost(feedPost);
-//
-//        return new ResponseEntity<>(
-//                new SingleResponseDto<>(mapper.feedToFeedResponseDto(updateFeedPost)),
-//                HttpStatus.OK);
-//    }
 
     @DeleteMapping("/{feed_id}")
     public ResponseEntity deleteFeedPost(@PathVariable("feed_id") @Positive int feedPostId) {
--- server/demo/src/main/java/example/domain/feedPost/service/feedPostService.java
+++ server/demo/src/main/java/example/domain/feedPost/service/feedPostService.java
@@ -4,8 +4,13 @@ import example.global.exception.BusinessLogicException;
 import example.global.exception.ExceptionCode;
 import example.domain.feedPost.entity.FeedPost;
 import example.domain.feedPost.repository.feedPostRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.text.DecimalFormat;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -24,6 +29,14 @@ public class feedPostService {
     public FeedPost findFeedPost(int feedId){
         Optional<FeedPost> optionalPost = feedPostRepository.findById(feedId); // findById : Optional<T> 객체 반환
         return optionalPost.orElseThrow(() -> new BusinessLogicException(ExceptionCode.FEEDPOST_NOT_FOUND));
+    }
+
+    // 최신글 순으로 조회
+    @Transactional(readOnly = true)
+    public Page<FeedPost> findAllFeedPost(int page, int size){
+        Page<FeedPost> feedPosts = feedPostRepository.findAll((PageRequest.of(page, size, Sort.by("feedPostId").descending())));
+
+        return feedPosts;
     }
 
     public FeedPost updateFeedPost(FeedPost feedPost){
