50e494986c9ef689fd518e4f745c10d608374556
Choi-hyeona <xhrl523@naver.com>
2023-05-12 20:55:09 +0900

    docs: 겹쳤던 코드 제거

--- client/src/components/Feed/FeedMaterial/Gradation.jsx
+++ client/src/components/Feed/FeedMaterial/Gradation.jsx
@@ -10,7 +10,6 @@ const RealGradation = styled.div`
   width: 100%;
   height: 135px;
   /* background 색상은 나중에 props로 바꾸어주어야 할 부분임 */
-  /* background: linear-gradient(to top, transparent, #c9edff); */
   background: ${({ gradColor }) =>
     gradColor ? `linear-gradient(to top, transparent, ${gradColor[2]})` : `linear-gradient(to top, transparent, #c9edff)`};
 `;
