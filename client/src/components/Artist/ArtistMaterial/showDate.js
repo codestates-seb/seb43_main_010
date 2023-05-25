3a12bf39c3d42dc72a9a4ed27c03f0fe1d6dc8fd
Joe-wonho <whdnjsgh22@naver.com>
2023-05-25 17:35:18 +0900

    fix: 시간 수정

--- client/src/components/Artist/ArtistMaterial/showDate.js
+++ client/src/components/Artist/ArtistMaterial/showDate.js
@@ -23,14 +23,3 @@ const showMonthDay = (a) => {
 };
 
 export { showDate, showMonthDay };
-
-// let t = '2023-05-25t03:02:17.773668';
-// console.log(new Date());
-// const now = new Date(); // 현재 시간
-// const utcNow = now.getTime() + now.getTimezoneOffset() * 60 * 1000; // 현재 시간을 utc로 변환한 밀리세컨드값
-// const koreaTimeDiff = 9 * 60 * 60 * 1000; // 한국 시간은 UTC보다 9시간 빠름(9시간의 밀리세컨드 표현)
-// const koreaNow = new Date(utcNow + koreaTimeDiff); // utc로 변환된 값을 한국 시간으로 변환시키기 위해 9시간(밀리세컨드)를 더함
-// console.log(utcNow);
-// console.log(new Date());
-// // console.log(utcNow + koreaTimeDiff);
-// // console.log(new Date(utcNow + koreaTimeDiff));
